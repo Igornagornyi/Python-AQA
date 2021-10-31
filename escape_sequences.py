afc14c5db08b24f4293bb2c2166d57ecb6a633f9
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2021-10-31 21:23:36 +0200

    added table

--- escape_sequences.py
+++ escape_sequences.py
@@ -1,1 +1,10 @@
-help('\\')
+from tabulate import tabulate
+
+column_name = ['value', 'escape sequencies']
+value = [['\\a', 'Bell (alert)'], ['\\b', 'Backspace'],
+               ['\\n', 'New line'], ['\\t', 'Horizontal tab'],
+               ["\\", "Backslash \ "], ['\\"', 'Double quotation mark "'],
+                ["\\'",   "Single quotation mark '"]]
+
+print(tabulate(value, column_name, tablefmt="grid"))
+
