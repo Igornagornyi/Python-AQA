46a51c037cbc6effe078144e60a136195aa7922b
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2021-11-11 22:00:44 +0200

    new commit

--- files.py
+++ files.py
@@ -1,74 +1,44 @@
 import re
-import requests
 
-# url = "https://rozetka.com.ua/ua/sport-i-uvlecheniya/"
+# Шаблон для названий разделов
+section_pattern = re.compile('>\s?(.+?)\s?</a>')
 
-# def get_response(url):
-    # respond_text = ''
-    # section_tag = 'class="tile-cats__heading ng-star-inserted'
-    # respond = requests.get(url)
-    # sections_dict = {}
+# Шаблон для назв. подразделов и url
+subsection_pattern = re.compile('a _ngcontent-sc.+?href="(.+?)".+?class="">\s?(.+?)\s?</a>')
 
-    #Шаблон для нахождения разделов розетка
-def get_section_pattern():
-    section_pattern = re.compile('>\s?(.+?)\s?</a>')
-    return section_pattern
 
-    #Шаблон для нахождения подразделов и url розетка
-def get_subsection_pattern():
-    subsection_pattern = re.compile('a _ngcontent-sc.+?href="(.+?)".+?class="">\s?(.+?)\s?</a>')
-    return subsection_pattern
-
-    #Нахождение разделов розетка
-def get_section_found():
-    with open('rozetka.html', encoding='utf-8') as f:
+# Сбор данных для словаря
+def complete_dict():
+    with open('rozetka.html', encoding='utf8') as f:
+        sections_dict = {}
         for section_text in f.read().split('class="tile-cats__heading ng-star-inserted"'):
-            section_found = get_section_pattern().search(section_text)
-        return section_found
+            section_found = section_pattern.search(section_text)
+            if section_found:
+                section_data = []
+                for subsection in subsection_pattern.finditer(section_text):
+                    if subsection:
+                        section_data.append(subsection.groups())
+                if section_data:
+                    sections_dict.setdefault(section_found.groups()[0], tuple(section_data))
+                section_data.clear()
+        return sections_dict
 
-er = get_section_found()
-print(er)
+    # Запись данных розетки в файл
 
-    #Создание списка
-def get_section_data():
-    if get_section_found():
-        section_data = []
-        return section_data
-
-eo = get_section_data()
-print(eo)
-
-    #Получение названий подразделов и url для словаря
-def get_subsection_titles_href():
-    with open('rozetka.html', encoding='utf-8') as f:
-        for section_text in f.read().split('class="tile-cats__heading ng-star-inserted"'):
-            for subsection in get_subsection_pattern().finditer(section_text):
-                if subsection:
-                    get_section_data().append(subsection.groups())
-            return get_section_data()
-
-nm = get_subsection_titles_href()
-print(nm)
 
 def write_file():
-    with open('rozetka.html', encoding='utf-8') as f:
-        sections_dict = {}
-        if get_subsection_titles_href():
-            sections_dict.setdefault(get_section_found().groups()[0], tuple(get_section_data()))
-            get_section_data().clear()
-            with open('files.txt', 'w') as f:
-                for k, v in sections_dict.items():
-                    f.writelines('\n')
-                    f.writelines(k.center(120, '-'))
-                    f.writelines('\n')
-                    for i in v:
-                        f.writelines('\n')
-                        f.writelines(i[1].ljust(50, '.'))
-                        f.writelines(i[0].rjust(50))
-                        f.writelines('\n')
+    with open('files.txt', 'w') as f:
+        for k, v in complete_dict().items():
+            f.write('\n')
+            f.write(k.center(120, '-'))
+            f.write('\n')
+            for i in v:
+                f.write('\n')
+                f.write(i[1].ljust(50, '.'))
+                f.write(i[0].rjust(50))
+                f.write('\n')
 
 
 if __name__ == '__main__':
     write_file()
 
-write_file()
