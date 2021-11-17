211b960934795a20939769878eee6f3bd0bd38bd
= <nikita.limarchuk@gmail.com>
2021-11-17 10:54:06 +0200

    add extension for 9 homework modules

---
+++ homework_09_1.py
@@ -0,0 +1,10 @@
+def get_square_data(a: int) -> tuple[int, float]:
+    """Calculate area of square, perimetr and diagonal"""
+    area_square = a**2
+    perimetr = a*4
+    diagonal = (a**2*2)**(1/2)
+
+    return (area_square, perimetr, float(f'{diagonal:.3f}'))
+
+
+c = 0
---
+++ homework_09_2.py
@@ -0,0 +1,17 @@
+
+def arithmetic(value1: int, value2: int, operation: str) -> int| float |str:
+    """Summerize, subtract, multiply, divide values"""
+    if operation == '+':
+        result = value1 + value2    
+    elif operation == '-':
+        result = value1 - value2
+    elif operation == '*':
+        result = value1*value2
+    elif operation == '/':
+        result = value1/value2
+    else:
+        result = f"Not known operation {operation}"
+
+    return result
+
+
