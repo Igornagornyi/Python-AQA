62b65b017e93891be1c88d0dbac95315504aa760
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2021-11-25 11:45:40 +0200

    Filter_func is modified

--- Homework_10/filter_func.py
+++ Homework_10/filter_func.py
@@ -9,23 +9,38 @@ list2 = [random.choices(string.ascii_letters,
          k=random.randint(1, 5)) for i in range(50)]
 
 
-def find_common_elem(list1: list, list2: list) -> list[list] | str:
+def find_common_elem(list1: list, list2: list): -> set[str]:
     """Find the same elements in both lists """
     my_list = []
     for i in list1:
         if i in list2:
-            my_list.append(i)
+            my_list.append(''.join(i))
+        result = set(my_list)
+    if result:
 
-    return my_list
+        return f"Вот общие элементы: {result}"
+    else:
+
+        return 'Cпробуй ще'
+
+
+
+
+
+
 
 
 ff = find_common_elem(list1, list2)
-# print(ff)
+print(ff)
 
-
-def filter_func(list1: list, list2: list) -> list[list] | str:
+#
+def filter_func(list1: list, list2: list): -> set[str]:
     """Find common elements in both lists"""
+    my_list = []
     result = list(filter(lambda i: i in list1, list2))
+    for i in result:
+        my_list.append(''.join(i))
+        result = set(my_list)
     if result:
 
         return f"Вот общие элементы: {result}"
@@ -34,4 +49,4 @@ def filter_func(list1: list, list2: list) -> list[list] | str:
         return 'Спробуй ще'
 
 
-# print(filter_func(list1, list2))
+print(filter_func(list1, list2))
