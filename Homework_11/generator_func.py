be2ee0703190da5ab2aa1f81f13dfbfe905abb92
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2021-11-26 14:00:10 +0200

    comments are added

--- Homework_11/generator_func.py
+++ Homework_11/generator_func.py
@@ -7,9 +7,10 @@ def raise_a_number(length: int) -> int:
 
 
 def show_numbers(number: int):
+    """Show all numbers in console"""
     for counter in raise_a_number(number):
         print(counter)
 
 
-nn = show_numbers(100)
+nn = show_numbers(1000000000)
 print(nn)
