c740231b3d17beb7f8269f88cc5b22beba738b8c
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2021-11-26 16:11:42 +0200

    decorator is modified

--- Homework_11/decorator_func.py
+++ Homework_11/decorator_func.py
@@ -1,7 +1,7 @@
 def print_file_name_func(function) -> str:
-    """Print name of the callable file plus arithm operation"""
+    """Print name of the callable file plus ariphm operation"""
     def decoratee(a, b):
-        return f"{function.__name__} {function(a, b)}"
+        return function.__name__, function(a, b)
 
     return decoratee
 
@@ -20,3 +20,4 @@ def mult_elements(a: int, b: int) -> int:
 
 print(sum_elements(3, 9))
 print(mult_elements(3, 10))
+print(mult_elements(4, 66))
