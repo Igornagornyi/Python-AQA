260a3b02c749077c38e17301250d0f01a5be7c69
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2021-11-29 21:04:04 +0200

    callback is modified

--- Homework_10/map_func.py
+++ Homework_10/map_func.py
@@ -2,12 +2,12 @@ from filter_func import list_elem
 from typing import List
 
 
-def implement_map_func(function,
+def implement_map_func(callback: callable,
                        sequence: List[str]) -> List[str]:
     """Add element 's' to str elements in the list"""
     my_list = []
     for item in sequence:
-        my_list.append(''.join(item))
+        my_list.append(callback(''.join(item)))
 
     return my_list
 
