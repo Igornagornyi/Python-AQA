4d9d9dc3de947ecd43fe01c8f02330a113b50bf8
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2021-11-29 20:49:07 +0200

    deleted extra elements

--- Homework_10/filter_func.py
+++ Homework_10/filter_func.py
@@ -5,19 +5,19 @@ from typing import List, Tuple
 
 # Create two lists with random str elements
 list_elem = [random.choices(string.ascii_letters,
-             k=random.randint(1, 5)) for i in range(100)]
+             k=random.randint(1, 5)) for i in range(50)]
 
 
-def implement_filter_func(function,
+def implement_filter_func(callback: callable,
                           sequence: List[str]) -> Tuple[str]:
     """Find elements in the list which count > 1"""
     my_list = []
     for item in sequence:
-        my_list.append(''.join(item))
-        result = set(my_list)
+        if callback(item):
+            my_list.append(''.join(item))
 
-    return result
+    return set(my_list)
 
 
-print(implement_filter_func(lambda element:
-                            list_elem.count(element) > 1, list_elem))
+print(implement_filter_func(lambda item:
+                            list_elem.count(item) > 1, list_elem))
