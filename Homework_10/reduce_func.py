345d6c14af0de45da1f119d633895dc71e8f8f7b
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2021-12-02 21:35:19 +0200

    some modifications

--- Homework_10/reduce_func.py
+++ Homework_10/reduce_func.py
@@ -2,13 +2,13 @@ list = [3, 35, 97, 72, 19]
 
 
 def sum_operation(value, item):
-    """Function summarizes two elements"""
+    """Summarize two elements"""
     return value + item
 
 
 def implement_reduce_func(sum_operation,
                           value: int, List: int) -> int:
-    """Function summarizes elements in the list with each other"""
+    """Summarize elements in the list with each other"""
     for item in List:
         value = sum_operation(value, item)
     return value
