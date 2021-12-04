1ef8914029319e7e15a6ca20dbf3ec84d0eea736
NahornyiIhor <nahornyi.tester@gmail.com>
2021-12-04 13:10:30 +0200

    reduce is finally modified

--- Homework_10/reduce_func.py
+++ Homework_10/reduce_func.py
@@ -1,12 +1,13 @@
 from operator import add
+from typing import List
 
 
-def implement_reduce_func(callback: callable, value: int, List: int) -> int:
+def implement_reduce_func(callback: callable, items: List[int]) -> int:
     """Summarize elements in the list with each other"""
-    result = value
-    for item in List:
+    result = items[0]
+    for item in items:
         result = callback(result, item)
     return result
 
 
-print(implement_reduce_func(add, 0, [3, 35, 97, 72, 19]))
+print(implement_reduce_func(lambda a, b: a +b, [3, 35, 97, 72, 19]))
