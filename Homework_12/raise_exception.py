bae5d4504830b238bfd9a1097165e858945f12a9
NahornyiIhor <nahornyi.tester@gmail.com>
2021-12-05 21:39:17 +0200

    minor fixes

--- Homework_12/raise_exception.py
+++ Homework_12/raise_exception.py
@@ -1,12 +1,13 @@
-def check__amount_elements():
+def check__quantiy_elements():
     """Raise exception if length > 10"""
     int_list = [2, 4, 7, 8, 3]
     while len(int_list) < 10:
-        value = input('Введите число: ')
-        int_list.append(int(value))
+        value = int(input('Введите число: '))
+        int_list.append(value)
 
     else:
+        print(int_list)
         raise Exception('You have already added 10 elements')
 
 
-check__amount_elements()
+check__quantiy_elements()
