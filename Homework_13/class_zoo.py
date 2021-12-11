4bc677637f5d9580b261c8b0b78152487c5108e2
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2021-12-11 13:16:02 +0200

    behavior is added

--- Homework_13/class_zoo.py
+++ Homework_13/class_zoo.py
@@ -1,12 +1,17 @@
 class Zoo:
-    def __init__(self, name: str, kingdom: str,
-                 order: str, family: str, gender: str, age: int) -> None:
+    def __init__(self, name: str, type: str,
+                 order: str, gender: str, age: int) -> None:
         self._name = name
-        self._kingdom = kingdom
+        self._type = type
         self._order = order
-        self._family = family
         self._gender = gender
         self._age = age
+
+    def return_age(self) -> str:
+        print(f"Now age is {self._age}")
+
+    def say_hello(self) -> str:
+        print(f'Hello {self._name}')
 
     @property
     def age(self) -> int:
@@ -16,13 +21,19 @@ class Zoo:
     def age(self, age: int) -> int:
         self._age = age
 
+    def age_once_increase(self) -> str:
+        print(f"Age is increased only once {self._age + 5}")
+
 
 if __name__ == '__main__':
 
-    class_examle = Zoo('chimpanzee', 'Animalia', 'Primates',
-                       'Hominidae', 'female', 18)
+    class_examle = Zoo('chimpanzee', 'monkey', 'Primates',
+                       'female', 18)
 
+    class_examle.say_hello()
 
-class_examle.age = 28
+    class_examle.age_once_increase()
 
-print(class_examle.age)
+    class_examle.age = 28
+
+    print(f"And now age is already {class_examle.age}")
