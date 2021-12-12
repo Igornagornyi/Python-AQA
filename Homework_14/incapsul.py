4cbeff65ec577cbe328fab696040c1f61caa14f4
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2021-12-12 10:53:04 +0200

    class weather is added

--- Homework_14/incapsul.py
+++ Homework_14/incapsul.py
@@ -8,11 +8,18 @@ class Time(Enum):
     night = 3
 
 
+class Weather(Enum):
+    sunny = 0
+    cold = 1
+    rainy = 2
+    hot = 3
+
+
 class Pet:
     def __init__(self, name) -> None:
         self.__name = name
 
-    def todo(self, time: Time) -> None:
+    def today(self, time: Time, weather: Weather) -> None:
         if time == Time.morning:
             print(f"It's time to walk with {self.__name}")
         elif time == Time.lunch:
@@ -21,11 +28,17 @@ class Pet:
             print(f"It's time for evening walk with {self.__name}")
         elif time == Time.night:
             print(f"It's time to sleep for {self.__name}")
-        else:
-            raise Exception('Time not defined')
+        if weather == Weather.sunny:
+            print("It's sunny today")
+        elif weather == Weather.cold:
+            print("It's cold outside")
+        elif weather == Weather.rainy:
+            print("It's rainy outside")
+        elif weather == Weather.hot:
+            print("It's hot outside")
 
 
 if __name__ == '__main__':
 
     my_pet = Pet('Zak')
-    my_pet.todo(Time.night)
+    my_pet.today(Time.night, Weather.rainy)
