a09bfd33235784ac18cc9892a9e937ba7bb83018
NahornyiIhor <nahornyi.tester@gmail.com>
2021-12-21 22:29:43 +0200

    sys.platform check added

--- Unitests/HW9/unittest_lesson9.py
+++ Unitests/HW9/unittest_lesson9.py
@@ -34,7 +34,7 @@ class GetSquare(unittest.TestCase):
         self.assertIn('.', str(out[2]))
 
     @unittest.skipIf(sys.platform != 'win64', reason='Not supported')
-    def test_dot_in_skip2(self):
+    def test_dot_skip2(self):
         """Check dot in str"""
         out = get_square_data(3)
         self.assertIn('.', str(out[2]))
