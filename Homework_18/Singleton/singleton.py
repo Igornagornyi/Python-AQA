aa05e214fb66b69021b8653576fa3044b146541d
NahornyiIhor <nahornyi.tester@gmail.com>
2022-01-07 22:03:58 +0200

    private method _instance

--- Homework_18/Singleton/singleton.py
+++ Homework_18/Singleton/singleton.py
@@ -3,10 +3,10 @@ from typing import Type
 
 def singleton(_type: Type):
     def decorate(name: str, id_number: int):
-        if hasattr(_type, "__instance"):
-            return getattr(_type, "__instance")
+        if hasattr(_type, "_instance"):
+            return getattr(_type, "_instance")
         else:
             instance = _type(name, id_number)
-            setattr(_type, "__instance", instance)
-            return getattr(_type, "__instance")
+            setattr(_type, "_instance", instance)
+            return getattr(_type, "_instance")
     return decorate
