6a8a6b026a6a8386977bade9504bde105cd0552f
NahornyiIhor <nahornyi.tester@gmail.com>
2022-01-08 10:18:40 +0200

    reversed() added to train, train prints objects

--- Homework_18/Train/train.py
+++ Homework_18/Train/train.py
@@ -9,24 +9,21 @@ class Train:
         self.__destination = 'Kyiv'
 
     def add_traincar(self, car: TrainCar) -> None:
-        return self.__cars.append(car.get_number())
+        return self.__cars.append(car)
 
     def add_traincars(self, cars: List[TrainCar]) -> List[TrainCar]:
         for car in cars:
-            self.__cars.append(car.get_number())
+            self.__cars.append(car)
         return self.__cars
 
     def __len__(self) -> int:
         return len(self.__cars)
 
     def __str__(self) -> str:
-        asc_order = ""
-        desc_order = ""
-        max_number = max(self.__cars)
-        sorted_cars = sorted(self.__cars)
-        reversed_cars = sorted(self.__cars, reverse=True)
-        for item in sorted_cars[0:-1]:
-            asc_order += f"{[item]}-"
-        for rev_item in reversed_cars[1:len(reversed_cars)]:
-            desc_order += f"-{[rev_item]}"
-        return f"<=[HEAD]-{asc_order}{[max_number]}{desc_order}"
+        right_order = ""
+        reverse_order = ""
+        for car in self.__cars[0:-1]:
+            right_order += f"{[car]}-"
+        for reverse_car in reversed(self.__cars):
+            reverse_order += f"-{[reverse_car]}"
+        return f"<=[HEAD]-{right_order}{reverse_order}"
