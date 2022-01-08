a7dd28af14b61529238d864a3e251f49c8c8202f
NahornyiIhor <nahornyi.tester@gmail.com>
2022-01-08 11:01:53 +0200

    attributes list renamed

--- Homework_18/Factory/main.py
+++ Homework_18/Factory/main.py
@@ -1,8 +1,8 @@
 from market import Market
 
 if __name__ == "__main__":
-    attributes_list = [input("Enter product color(green, yellow, red): "),
-                       input("Enter product shape(round, tapered, prolonged, herbaceous): "),
-                       input("Enter product size(small or big): ")]
-    prod = Market.get_product(attributes_list)
+    attributes = [input("Enter product color(green, yellow, red): "),
+                  input("Enter product shape(round, tapered, prolonged, herbaceous): "),
+                  input("Enter product size(small or big): ")]
+    prod = Market.get_product(attributes)
     print(prod)
