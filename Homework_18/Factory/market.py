1d4ad313be4f415e5d365cde0c1959ed742764ff
NahornyiIhor <nahornyi.tester@gmail.com>
2022-01-08 11:03:51 +0200

    List from typing added

--- Homework_18/Factory/market.py
+++ Homework_18/Factory/market.py
@@ -3,11 +3,12 @@ from Homework_18.Factory.bannana import Bannana
 from Homework_18.Factory.cellery import Cellery
 from strawberry import Strawberry
 from product import Product
+from typing import List
 
 
 class Market:
     @staticmethod
-    def get_product(values) -> Product:
+    def get_product(values: List[str]) -> Product:
         """Return product name by attributes"""
         if all([value in Apple().attributes for value in values]):
             return Apple()
