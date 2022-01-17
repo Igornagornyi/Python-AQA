56873306255950416dd3bd7501e58a720a541ad9
NahornyiIhor <nahornyi.tester@gmail.com>
2022-01-17 23:00:03 +0200

    click buttons, search field, scrolling

--- Homework_20_selenium_wd/main.py
+++ Homework_20_selenium_wd/main.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from time import sleep
 
-
 driver = Chrome("./chromedriver")
 driver.get("https://www.motorsport.com/")
 driver.maximize_window()
@@ -18,7 +17,9 @@ filter_button: WebElement = driver.find_element(
     "//div[@class='ms-filter_toggler']")
 filter_button.click()
 sleep(5)
-search_field: WebElement = driver.find_element(By.XPATH, "//input[@class='ms-filter-search_input']")
+search_field: WebElement = driver.find_element(
+    By.XPATH,
+    "//input[@class='ms-filter-search_input']")
 search_field.send_keys("Formula")
 sleep(5)
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
